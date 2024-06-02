f74975f20add0d8043a8af6fec00e33d9ca6bbf8
Orel <orelg@cymulate.com>
2024-06-03 00:33:15 +0300

    WIP

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,22 +1,36 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHttpsRedirection(options =>
 {
+    options.RedirectStatusCode = StatusCodes.Status308PermanentRedirect;
     options.HttpsPort = 443;
 });
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
 app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
